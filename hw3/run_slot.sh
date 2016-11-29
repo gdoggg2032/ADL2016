b38bd972e3f01b4871f89f2c68e8efa49b1515e7
gdoggg <gdoggg2032@gmail.com>
2016-11-29 22:02:15 +0800

    run_slot.sh: parameters

--- hw3/run_slot.sh
+++ hw3/run_slot.sh
@@ -7,7 +7,7 @@ task=tagging  # available options: intent; tagging; joint
 bidirectional_rnn=True  # available options: True; False
 mode=test
 answer=$2
-word_embedding_size=300
+
 
 CUDA_VISIBLE_DEVICES="" python run_multi-task_rnn.py --data_dir $data_dir \
       --train_dir   $train_dir\
@@ -15,7 +15,6 @@ CUDA_VISIBLE_DEVICES="" python run_multi-task_rnn.py --data_dir $data_dir \
       --task $task \
       --bidirectional_rnn $bidirectional_rnn \
       --mode $mode \
-      --answer $answer \
-	  --word_embedding_size $word_embedding_size
+      --answer $answer 
 
 python pred_format.py --input $answer --output $answer
